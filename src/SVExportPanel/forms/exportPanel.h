5b19082ca712825868fc46bda05ff3121070cd7d
al_medved <al_medved@mail.ru>
2018-09-11 23:14:13 +0500

    -add export panel project

---
+++ src/SVExportPanel/forms/exportPanel.h
@@ -0,0 +1,88 @@
+//
+// SVisual Project
+// Copyright (C) 2018 by Contributors <https://github.com/Tyill/SVisual>
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#pragma once
+
+#include "src/stdafx.h"
+#include "ui_exportPanel.h"
+#include "SVConfig/SVConfigData.h"
+#include "SVExportPanel.h"
+
+class exportPanel : public QDialog
+{
+	Q_OBJECT
+
+public:
+
+	SV_Exp::pf_getCopySignalRef pfGetCopySignalRef = nullptr;
+	SV_Exp::pf_getSignalData pfGetSignalData = nullptr;
+	SV_Exp::pf_loadSignalData pfLoadSignalData= nullptr;
+	SV_Exp::pf_setTimeInterval pfSetTimeInterval = nullptr;
+	SV_Exp::pf_getTimeInterval pfGetTimeInterval = nullptr;
+
+    exportPanel(QWidget *parent, SV_Exp::config);
+    ~exportPanel();
+	
+private:
+	Ui::ExportPanelClass ui;
+	
+    SV_Exp::config cng;
+
+	struct valSData{
+		int changeCnt;
+		int duration;
+		int durationMean;
+	};
+
+	struct graphSignData{
+		QString sign;
+		QString name;	
+		QMap<int, valSData> valData;
+		QVector<QPair<int, int>> hist;
+	};
+
+			
+	bool diapEna_ = false;
+
+	QMap<QString, graphSignData> sign_;
+	QVector<SV_Cng::recData> vars_;
+
+	QVector<SV_Cng::recData> getSignData(QString sign);
+	void listTimeUpdate(QString sname, int val);
+	QVector<QPair<int, int>> calcHist(QString sname);
+
+	// Mx Hist
+	int Mx(QVector<QPair<int, int>>& hist);
+	
+private slots:
+	void dragEnterEvent(QDragEnterEvent *event);
+	void dragMoveEvent(QDragMoveEvent *event);
+	void dropEvent(QDropEvent *event);
+	void graphValueChange();
+	void selectSignalChange();
+	void selectSignalTime(int row);
+	
+};
+
+
